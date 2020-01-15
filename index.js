acea4346898ac3ec07ef7a0dc14ca997fdc2d113
Westley Feller <feller.westley@gmail.com>
2020-01-15 13:29:58 -0800

    Get user by ID

--- index.js
+++ index.js
@@ -127,3 +127,25 @@ server.get('/users', (_req, res) => {
       });
     });
 });
+
+server.get('/users/:id', (req, res) => {
+  users
+    .findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.status(200).json({ success: true, user });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: 'The user with the specified ID does not exist.',
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        errorMessage: 'The user information could not be retrieved.',
+        error,
+      });
+    });
+});
